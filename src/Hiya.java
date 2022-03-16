8d10f64d1f56f976b6fff9994a4bf8748c5b6023
audreytabler <audreytabler@gmail.com>
2022-03-16 01:06:23 +0000

    2

--- src/Hiya.java
+++ src/Hiya.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Hiya {
     public static void main(String[] args) {
-        System.out.println("hi!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("hello there!");
+        System.out.print("Enter a String: ");
+        String hi = input.next();
+
+        for(int i = 0; i<5; i++){ System.out.print(hi + " ");}
+
+        System.out.print("Can you see this Caleb?? :eyes:");
+        
+        System.out.println("Does this work?");
+        System.out.println("Yeah!!");
+        WOOOOO HYPE
+        Systme
+
     }
 }
